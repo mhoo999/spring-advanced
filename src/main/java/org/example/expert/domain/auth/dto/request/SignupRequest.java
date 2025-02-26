416257f4e2d8a1921ba1f203da871f56852534f3
myeonghoon <mhoo999@naver.com>
2025-02-26 17:04:52 +0900

    레벨 1-1: Early Return

--- src/main/java/org/example/expert/domain/auth/dto/request/SignupRequest.java
+++ src/main/java/org/example/expert/domain/auth/dto/request/SignupRequest.java
@@ -2,6 +2,8 @@ package org.example.expert.domain.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,8 +15,10 @@ public class SignupRequest {
 
     @NotBlank @Email
     private String email;
+
     @NotBlank
     private String password;
+
     @NotBlank
     private String userRole;
 }
